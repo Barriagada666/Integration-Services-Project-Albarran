fa71938f4710a259ed5734f4629b2eb994bca96b
unknown <Duoc@VIAPCE03LR0105.duoc.acad>
2024-11-14 20:07:46 -0300

    Cubo olap funcionando

---
+++ USUARIO OLAP.sql
@@ -0,0 +1,12 @@
+USE [master]
+GO
+
+/****** Object:  Login [NT SERVICE\MSSQLServerOLAPService]    Script Date: 14/11/2024 19:59:42 ******/
+DROP LOGIN [NT SERVICE\MSSQLServerOLAPService]
+GO
+
+/****** Object:  Login [NT SERVICE\MSSQLServerOLAPService]    Script Date: 14/11/2024 19:59:42 ******/
+CREATE LOGIN [NT SERVICE\MSSQLServerOLAPService] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[Español]
+GO
+
+
